17e46ed58bf28dda8e5f2622ded1a7089f14eb6c
Rubens Albukrk <rubensf4rda@gmail.com>
2023-12-27 02:08:02 -0300

    test filhos object

---
+++ prisma/migrations/20231227050709_/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - The `filhos` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "filhos",
+ADD COLUMN     "filhos" JSONB[];
