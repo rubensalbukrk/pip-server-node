6e0a434e458f14e66468795836c1f47733de7360
Rubens Albukrk <rubensf4rda@gmail.com>
2024-01-02 16:42:58 -0300

    stable

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ import app from "./src/app"
 const port = process.env.PORT || 5500
 
 app.listen(port, () => {
-  console.log(`Servidor online em http://localhost:${port}`)
+  console.log(`Servidor ligado http://localhost:${port}`)
 })
